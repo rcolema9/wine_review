b1e0b6cb1f2f2f50736f8b0a57da3e4e74d41935
Richard Coleman <coleman_richard2@bah.com>
2014-04-13 18:56:11 -0400

    Homework # 9 Complete Message

--- app/controllers/wines_controller.rb
+++ app/controllers/wines_controller.rb
@@ -1,5 +1,7 @@
 class WinesController < ApplicationController
 
+  before_action :set_wine, only: [ :show, :edit, :update, :destroy ]
+
   def index
     @available_at = Time.now
     @wines = Wine.all
@@ -8,6 +10,47 @@ class WinesController < ApplicationController
    end
 
    def show
-     @wine = Wine.find(params[:id])
+     #Removed when adding "Drying Out" code
+     #@wine = Wine.find(params[:id])
    end
+
+  def new
+  	@wine = Wine.new
+  end
+
+  def create
+  	@wine = Wine.new(wine_params)
+  	@wine.save
+  	redirect_to @wine
+  end
+
+  def edit
+	#Removed when adding "Drying Out" code
+	#@wine = Wine.find(params[:id])
+  end
+
+  def update
+    #Removed when adding "Drying Out" code
+    #@wine = Wine.find(params[:id])
+    @wine.update(wine_params)
+    redirect_to @wine
+  end
+
+  def destroy
+  	#Removed when adding "Drying Out" code
+  	#@wine = Wine.find(params[:id])
+  	@wine.destroy
+  	redirect_to wines_url
+  end
+
+  private
+
+  def wine_params
+    params.require(:wine).permit(:name, :year, :winery, :country, :varietal)
+  end
+
+  def set_wine
+  	@wine = Wine.find(params[:id])
+  end
+
 end
